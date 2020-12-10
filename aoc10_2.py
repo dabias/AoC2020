83b8d5fb464ebfe4313ce0c8ba40f1d0995c8ca7
Dabias <douwe.brinkhorst@gmail.com>
2020-12-10 17:48:54 +0100

    Day 10.2 passes final test

--- aoc10_2.py
+++ aoc10_2.py
@@ -17,7 +17,6 @@ def findPermutations(diffList, index):
     if diffList[index + 1] < 3:
         count += findPermutations(diffList, index + 1)
         if (diffList[index] + diffList[index + 1]) <= 3:
-            print('permutations found at index: ' + str(index))
             newDiffList = []
             for item in diffList:
                 newDiffList.append(item)
@@ -32,9 +31,7 @@ firstIteration = True
 preceding3 = False
 for i in range(len(joltDiff)-1):
     if firstIteration or (preceding3 and joltDiff[i] < 3):
-        print('starting point found at index: ' + str(i))
         permutations = permutations*findPermutations(joltDiff, i)
-    print(preceding3)
     firstIteration = False
     if joltDiff[i] == 3:
         preceding3 = True
