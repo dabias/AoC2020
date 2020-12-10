c6f7db64b9990f383d0556e86e5009edbe70f33f
Dabias <douwe.brinkhorst@gmail.com>
2020-12-10 17:46:32 +0100

    Day 10.2 passes first and second example

--- aoc10_2.py
+++ aoc10_2.py
@@ -5,26 +5,41 @@ with open('aoc10.txt') as f:
         adapters.append(int(line))
 
 adapters.sort()
-adapters.append(adapters[-1]+3)
+adapters.append(adapters[-1] + 3)
 
 joltDiff = [None]*(len(adapters)-1)
 
 for i in range(len(adapters)-1):
     joltDiff[i] = adapters[i+1]-adapters[i]
 
-def findPermutations(diffList,index, depth):
-    count = 1
-    while diffList[index] < 3:
-        print('start investigating index: ' + str(index) + ' at depth: ' + str(depth))
-        if (diffList[index+1] ) == 1:
-            print('permutation found at index: ' + str(index))
-            newDiffList = diffList
-            newDiffList[index] += newDiffList[index+1]
+def findPermutations(diffList, index):
+    count = 0
+    if diffList[index + 1] < 3:
+        count += findPermutations(diffList, index + 1)
+        if (diffList[index] + diffList[index + 1]) <= 3:
+            print('permutations found at index: ' + str(index))
+            newDiffList = []
+            for item in diffList:
+                newDiffList.append(item)
+            newDiffList[index] += newDiffList[index + 1]
             del newDiffList[index+1]
-            count += findPermutations(newDiffList, index, depth + 1)
-    return count
+            count += findPermutations(newDiffList, index)
+        return count
+    else:
+        return 1
 
+firstIteration = True
+preceding3 = False
 for i in range(len(joltDiff)-1):
-    if (i == 0 or ((joltDiff[i-1] == 3 and joltDiff[i] < 3))):
-        permutations = permutations*findPermutations(joltDiff, i,0)
+    if firstIteration or (preceding3 and joltDiff[i] < 3):
+        print('starting point found at index: ' + str(i))
+        permutations = permutations*findPermutations(joltDiff, i)
+    print(preceding3)
+    firstIteration = False
+    if joltDiff[i] == 3:
+        preceding3 = True
+    else:
+        preceding3 = False
 print(permutations)
+
+
